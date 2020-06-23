05e5605fbaac0ad8aeb3ce1e776d1754560fd406
Joshua Poddoku <201751033@iiitvadodara.ac.in>
2020-06-23 15:44:26 +0530

    added key

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -3,7 +3,7 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
